e7b40d6b6a772e573b79121b043d422a4b3b275b
Ahmed Semih Erkan <ahmedsemiherkan@gmail.com>
2023-01-31 17:35:01 +0300

    feat: create todo page

---
+++ Models/TodoViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace fullstack_todo_app.Models
+{
+    public class TodoViewModel
+    {
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Todo is required.")]
+        [MaxLength(50, ErrorMessage = "Todo must be max 50 character.")]
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        public bool Status { get; set; } = false;
+        public DateTime? Deadline { get; set; }
+    }
+}
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,11 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿const checkBoxes = document.querySelectorAll(".checkboxes");
+const submitBtn = document.getElementById("submitBtn");
 
-// Write your JavaScript code.
+checkBoxes.forEach((checkbox) => {
+    checkbox.addEventListener("click", () => {
+        const button = document.getElementById(`submitBtn_${checkbox.id}`);
+        setTimeout(() => {
+            button.click();
+        }, 2000);
+    });
+});
